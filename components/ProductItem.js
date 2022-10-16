b0c5e3a42778f3619a58ee94ee23ae14281e7f43
fatihyanik <fatihyanik07@gmail.com>
2022-10-16 16:12:51 +0200

    load products from mongodb

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-function ProductItem({ product }) {
+function ProductItem({ product, addToCartHandler }) {
     return (
         <div className="card">
             <Link href={`/product/${product.slug}`}>
@@ -22,7 +22,11 @@ function ProductItem({ product }) {
                 </Link>
                 <p className="mb-2">{product.brand}</p>
                 <p>${product.price}</p>
-                <button className="primary-button" type="button">
+                <button
+                    className="primary-button"
+                    type="button"
+                    onClick={() => addToCartHandler(product)}
+                >
                     Add to cart
                 </button>
             </div>
--- utils/db.js
+++ utils/db.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 
 const connection = {};
 
-async function connect(){
-    if(connection.isConnected){
-       console.log('already connected');
-       return; 
+async function connect() {
+    if (connection.isConnected) {
+        console.log('already connected');
+        return;
     }
-    if(mongoose.connections.length >0){
+    if (mongoose.connections.length > 0) {
         connection.isConnected = mongoose.connections[0].readyState;
-        if(connection.isConnected === 1){
+        if (connection.isConnected === 1) {
             console.log('use previous connection');
             return;
         }
@@ -20,17 +20,24 @@ async function connect(){
     connection.isConnected = db.connections[0].readyState;
 }
 
-async function disconnect(){
-    if(connection.isConnected){
-        if(process.env.NODE_ENV === 'production'){
+async function disconnect() {
+    if (connection.isConnected) {
+        if (process.env.NODE_ENV === 'production') {
             await mongoose.disconnect();
             connection.isConnected = false;
-        }else{
+        } else {
             console.log('not disconnected');
         }
     }
 }
 
+function convertDocToObj(doc) {
+    doc._id = doc._id.toString();
+    doc.createdAt = doc.createdAt.toString();
+    doc.updatedAt = doc.updatedAt.toString();
+    return doc;
+}
 
-const db = { connect, disconnect };
+
+const db = { connect, disconnect, convertDocToObj };
 export default db;
